2120a483e21463289b278a56cbd80e683246612b
oswaldoroman <edwin.roman@ucb.edu.bo>
2023-08-17 09:58:54 -0400

    Se añadio la funcion de reconocer fizzbuzz

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,11 +1,15 @@
 function fizzbuzz(a) {
-  if (a % 3 == 0) {
-    return "Fizz";
+  if (a % 3 == 0 && a % 5 == 0) {
+    return "FizzBuzz";
   } else {
-    if (a % 5 == 0) {
-      return "Buzz";
+    if (a % 3 == 0) {
+      return "Fizz";
     } else {
-      return a;
+      if (a % 5 == 0) {
+        return "Buzz";
+      } else {
+        return a;
+      }
     }
   }
 }
--- src/fizzbuzz.test.js
+++ src/fizzbuzz.test.js
@@ -1,14 +1,21 @@
 import fizzbuzz from "./fizzbuzz";
 
 describe("FizzBuzz", () => {
+
   it("deberia retornar el numero", () => {
     expect(fizzbuzz(4)).toEqual(4);
   });
+
   it("deberia retornar el fizz", () => {
     expect(fizzbuzz(3)).toEqual("Fizz");
   });
+
   it("deberia retornar el buzz", () => {
     expect(fizzbuzz(5)).toEqual("Buzz");
+  });
+  
+  it("deberia retornar el fizzbuzz", () => {
+    expect(fizzbuzz(15)).toEqual("FizzBuzz");
   });
 
 });
